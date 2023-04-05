e561a94b36fcfb4f099e80549c84a8b8c2a7575b
Princekotadiya007 <princuuu77@gmail.com>
2023-04-05 18:03:46 +0530

    [prince] checking equal

--- LineCompersionProblem/LineCompersionProblem/Program.cs
+++ LineCompersionProblem/LineCompersionProblem/Program.cs
@@ -10,9 +10,22 @@ namespace LineCompersionProblem
     {
         static void Main(string[] args)
         {
-            LineCompersion line = new LineCompersion(1,2,3,4);
-            line.Length();
-            Console.ReadLine();
+            LineCompersion line = new LineCompersion(1, 2, 3, 4);
+            double length1 = line.Length();
+            Console.WriteLine("Length of 1st Line : " + length1);
+
+            LineCompersion line2 = new LineCompersion(5, 6, 7, 9);
+            double length2 = line2.Length();
+            Console.WriteLine("Length of 2nd Line : " + length2);
+
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("Both Line Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Both Line Are not Equal");
+            }
         }
     }
 }
