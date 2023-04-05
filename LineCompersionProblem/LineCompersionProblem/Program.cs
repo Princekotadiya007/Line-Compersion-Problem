41752b7cde7a2d9ed0da4ac1641b6fbd9666fc65
Princekotadiya007 <princuuu77@gmail.com>
2023-04-05 17:48:28 +0530

    [prince] Length of line

---
+++ LineCompersionProblem/LineCompersionProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineCompersionProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LineCompersion line = new LineCompersion(1,2,3,4);
+            line.Length();
+            Console.ReadLine();
+        }
+    }
+}
